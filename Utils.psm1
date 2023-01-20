605862b7c393385266419b46f2409a5e019391c2
aetonsi <18366087+aetonsi@users.noreply.github.com>
2023-01-20 15:24:25 +0100

    add util confirm

--- Utils.psm1
+++ Utils.psm1
@@ -1,3 +1,6 @@
+using namespace System.Management.Automation.Host;
+using namespace System.Collections.ObjectModel;
+
 function Test-PendingReboot {
   # https://stackoverflow.com/a/43596428/9156059
   # https://gist.github.com/altrive/5329377
@@ -15,5 +18,22 @@ function Test-PendingReboot {
   catch {}
   return $false
 }
+
+
+# TODO #conf1: implement OPTIONAL ref parameter $choiceMade
+# TODO #conf1 NEW usage example: $confirmation = confirm 'Overwrite destination file: profile.ovpn' 'File already exists in destination folder. It will be overwritten. Do you want to continue?' ([System.Management.Automation.Host.ChoiceDescription]::new('&Yes','Overwrite the file.'), [System.Management.Automation.Host.ChoiceDescription]::new('&No','Cancel the operation and halt the script.'), [System.Management.Automation.Host.ChoiceDescription]::new('See &more options','See additional options (renaming the file, moving it, etc)'))
+# usage example: $confirmation = confirm 'Overwrite destination file: profile.ovpn' 'File already exists in destination folder. It will be overwritten. Do you want to continue?' ([System.Management.Automation.Host.ChoiceDescription]::new('&Yes','Overwrite the file.'), [System.Management.Automation.Host.ChoiceDescription]::new('&No','Cancel the operation and halt the script.'))
+function confirm(
+  [string] $title = '',
+  [string] $prompt,
+  [Collection[ChoiceDescription]] $choices = ('&Yes', '&No'),
+  [int] $default = -1
+  # [ref][Parameter(Mandatory = $false)][AllowNull()][int] $choiceMade = $null) # TODO #conf1
+) {
+  $decision = $Host.UI.PromptForChoice($title, $prompt, $choices, $default)
+  # if ($choiceMade -is [ref]) { $choiceMade.value = $decision } # TODO #conf1
+  return $decision -eq 0
+}
+
 
 Export-ModuleMember -Function *
